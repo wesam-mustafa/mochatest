239c85ed8bc6ca404b2a677d7f9d99c4a7f2d6ae
wesam mustafa <wesam.mustafa100@gmail.com>
2017-12-06 03:39:30 +0200

    before linting and beautify

--- test/apptest.js
+++ test/apptest.js
@@ -1,9 +1,24 @@
 const assert = require('chai').assert;
-const sayHello=require('../app').sayHello;
+const sayHello = require('../app').sayHello;
+const addNumbers = require('../app').addNumbers;
 // const app = require('../app');
 describe('App', () => {
     it('sayHello should return hello', () => {
-        let result=sayHello();
+        let result = sayHello();
         assert.equal(result, 'Hello');
+    });
+
+    it("sayHello should return string", () => {
+        let result = sayHello();
+        assert.typeOf(result, 'string');
+    });
+
+    it("addNumbers Should return number above 5", () => {
+        let result = addNumbers(5, 5);
+        assert.isAbove(result, 5);
+    });
+    it("addNumbers should return type number", () => {
+        let result = addNumbers();
+        assert.typeOf(result, 'number');
     });
 });
